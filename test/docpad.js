ae4c9b828b02bb0dda604da8f6e474ace5e354b7
Benjamin Lupton <b@lupton.cc>
2017-03-29 12:27:58 +0800

    disable source maps for tests
    disable source maps for tests as source map results are specific to the
    particular environments, so balupton's sourcemap on his machine will be
    different to travis's source map, which will cause the test to fail

---
+++ test/docpad.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// disable source maps for tests as source map results are specific to the particular environments/
+// so balupton's sourcemap on his machine will be different to travis's source map
+// which will cause the test to fail
+module.exports = {
+	environments: {
+		development: {
+			plugins: {
+				less: {
+					lessOptions: {
+						sourceMap: false
+					}
+				}
+			}
+		}
+	}
+}
