e6ebc28eeadcfb2b47448805bfff0a0ad40dae68
Benjamin Lupton <b@lupton.cc>
2017-03-29 12:39:34 +0800

    v2.5.0. Improvements. (#17)
    Merges in the v2.5.0 changes into the current master:
    https://github.com/docpad/docpad-plugin-less/tree/8ee72b49e0bd2787b12eb408481ee55251fb0e25
    
    Squash merge to avoid comitting things that were not needed.
    
    @sdomagala's work:
    
    - less v1.6 to v2.6 upgrade
    - coffeescript to esnext conversion
    - update base files
    
    @balupton's modifications:
    
    - remove `/docs` as there are no docs for this plugin
    - update base files to latest conventions
    - linting fixes, including spaces to tabs
    - re-added the history to `HISTORY.md`
    - removed `docpad` from `engines` as it is in `peerDependencies` which
    is the new place
    - moved from `import` to `require` so the source can run on node
    - moved from `Object.assign` to `extendr` - docpad already uses extendr
    so this reduces complexity
    - update editions for latest standard and correct syntax
    - removed superfluous dev dependencies
    - fixed plugin configuration not being used - should be `get
    initialConfig` for ES6 classes
    - `paths` option was not an array when it should be, now fixed
    - the `Object.assign` was writing to the wrong object, as it should be
    updating `parseOptions` to send to the render
    - made the `compress` option useful again, as it became useless when
    `compileOptions` was removed
    - added source maps #10
    - added ability to disable referencesOthers #12
    - added tests to show the importance of referencesOthers, source maps,
    and compression
    - disable source maps for tests as the results will always be different
    
    Thanks everyone for your assistance on this. Greatly appreciated and
    well done.

---
+++ source/index.js
@@ -0,0 +1,69 @@
+'use strict'
+
+// Export Plugin
+module.exports = function (BasePlugin) {
+	// Define Plugin
+	return class LessPlugin extends BasePlugin {
+		// Plugin name
+		get name () {
+			return 'less'
+		}
+
+		// Plugin initial config
+		get initialConfig () {
+			return {
+				referencesOthers: true,
+				lessOptions: {
+					compress: true,
+					sourceMap: false
+				},
+				environments: {
+					development: {
+						lessOptions: {
+							compress: false,
+							sourceMap: {
+								sourceMapFileInline: true
+							}
+						}
+					}
+				}
+			}
+		}
+
+		// Render the extensions [css].less with less
+		render (opts, next) {
+			const { inExtension, outExtension, file } = opts
+
+			// Check extensions
+			if (inExtension === 'less' && ['css', null].indexOf(outExtension) !== -1) {
+				// Load the dependencies into memory now that we know we need them
+				const less = require('less')
+				const extendr = require('extendr')
+
+				// Prepare our parser options to send to less by merging what we want with what the user has provided
+				// http://lesscss.org/#using-less-configuration
+				const lessOptions = extendr.extend({
+					paths: [file.get('fullDirPath')],
+					filename: file.get('fullPath')
+				}, this.config.lessOptions || {})
+
+				// If the less file references something else
+				// then note that it references others
+				// so that if something changes, this will also rerender
+				// this is needed because less concats imports into one file
+				if (this.config.referencesOthers !== false && opts.content.indexOf('@import') !== -1) {
+					file.setMetaDefaults({ referenceOthers: true })
+				}
+
+				// now render the content with less css
+				// http://lesscss.org/#using-less-usage-in-code
+				less.render(opts.content, lessOptions, function (err, data) {
+					if (err) return next(err)
+					opts.content = data.css
+					next()
+				})
+			}
+		}
+	}
+
+}
