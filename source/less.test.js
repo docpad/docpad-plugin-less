bd9ab68b22232ee941a7de8386511230c6cdd37e
Benjamin Lupton <b@lupton.cc>
2020-08-06 08:44:42 +1000

    Update less.test.js

--- source/less.test.js
+++ source/less.test.js
@@ -1,16 +1,12 @@
 'use strict'
 
-require('docpad').require('testers')
+module.exports = require('docpad-plugintester')
 	// Development
 	.test({
-		pluginPath: `${__dirname}/..`,
-		testerClass: 'RendererTester',
 		outExpectedPath: `${__dirname}/../test/out-expected-development`
 	})
 	// Production
 	.test({
-		pluginPath: `${__dirname}/..`,
-		testerClass: 'RendererTester',
 		outExpectedPath: `${__dirname}/../test/out-expected-production`
 	}, {
 		env: 'production'
