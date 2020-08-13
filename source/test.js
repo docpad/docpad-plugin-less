13913f9fb843077d1dca2ae3f2c2bd1a14279be4
Benjamin Lupton <b@lupton.cc>
2020-08-13 12:21:28 +1000

    v1.7.1 - boundation, changelog, version bump

--- source/test.js
+++ source/test.js
@@ -1,13 +1,16 @@
+// @ts-nocheck
 'use strict'
 
 module.exports = require('docpad-plugintester')
 	// Development
 	.test({
+		testerName: 'less plugin: development',
 		outExpectedPath: `${__dirname}/../test/out-expected-development`,
 	})
 	// Production
 	.test(
 		{
+			testerName: 'less plugin: production',
 			outExpectedPath: `${__dirname}/../test/out-expected-production`,
 		},
 		{
